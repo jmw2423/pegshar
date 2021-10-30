16f35cd82b5e9c2159672be0875fb9ed5d54e4c3
rdl4199 <rdl4199@rit.edu>
2021-10-30 12:48:20 -0400

    FixedShooting
    Fixed shooting over the top

--- Peggle/Assets/scripts/gun.cs
+++ Peggle/Assets/scripts/gun.cs
@@ -87,12 +87,22 @@ public class gun : MonoBehaviour
         {
             transform.up = new Vector3(0, 0, 0);
         }*/
+        Vector2 direction = new Vector2(0.0f, 0.0f);
         Vector3 mousePosition = Input.mousePosition;
         mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-
-        Vector2 direction = new Vector2(
-        mousePosition.x - transform.position.x,
-        mousePosition.y - transform.position.y);
+        if(mousePosition.y > 5.0f)
+        {
+            direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            5.0f - transform.position.y);
+        }
+        else
+        {
+            direction = new Vector2(
+            mousePosition.x - transform.position.x,
+            mousePosition.y - transform.position.y);
+        }
+        
 
         
         transform.up = -direction;
